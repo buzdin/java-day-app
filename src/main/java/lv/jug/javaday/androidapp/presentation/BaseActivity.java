1efb5fd4c1d0a016b3aa698be89ef362ff09bce8
LArchaon <alex.buzdin@gmail.com>
2013-09-22 21:59:18 +0300

    Navigation drawer starts other activities

--- src/main/java/lv/jug/javaday/androidapp/presentation/BaseActivity.java
+++ src/main/java/lv/jug/javaday/androidapp/presentation/BaseActivity.java
@@ -1,9 +1,6 @@
 package lv.jug.javaday.androidapp.presentation;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.SearchManager;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -18,9 +15,11 @@ import butterknife.Views;
 import com.squareup.otto.Bus;
 import lv.jug.javaday.androidapp.R;
 import lv.jug.javaday.androidapp.infrastructure.BaseApplication;
+import lv.jug.javaday.androidapp.presentation.home.HomeActivity;
+import lv.jug.javaday.androidapp.presentation.schedule.ScheduleDashboardActivity;
+import lv.jug.javaday.androidapp.presentation.speaker.SpeakerDashboardActivity;
 
 import javax.inject.Inject;
-import java.util.Locale;
 
 public abstract class BaseActivity extends Activity {
 
@@ -33,13 +32,11 @@ public abstract class BaseActivity extends Activity {
     @InjectView(R.id.left_drawer)
     ListView drawerList;
 
-    private String[] navigationItems;
+    CharSequence title;
 
-    private CharSequence drawerTitle;
+    CharSequence drawerTitle;
 
-    private CharSequence title;
-
-    private ActionBarDrawerToggle drawerToggle;
+    ActionBarDrawerToggle drawerToggle;
 
     @Override protected void onCreate(Bundle state) {
         super.onCreate(state);
@@ -69,12 +66,16 @@ public abstract class BaseActivity extends Activity {
     protected void init(Bundle state){}
 
     private void initDrawerLayout(Bundle state) {
-        navigationItems = getResources().getStringArray(R.array.navigation_drawer);
+        String[] navigationItems = getResources().getStringArray(R.array.navigation_drawer);
 
-        drawerList.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, navigationItems));
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navigationItems));
         drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
 
         title = drawerTitle = getTitle();
 
@@ -92,19 +93,15 @@ public abstract class BaseActivity extends Activity {
         ) {
             public void onDrawerClosed(View view) {
                 getActionBar().setTitle(title);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                invalidateOptionsMenu();
             }
 
             public void onDrawerOpened(View drawerView) {
                 getActionBar().setTitle(drawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                invalidateOptionsMenu();
             }
         };
         drawerLayout.setDrawerListener(drawerToggle);
-
-        if (state == null) {
-            selectItem(0);
-        }
     }
 
     @Override
@@ -113,79 +110,31 @@ public abstract class BaseActivity extends Activity {
         getActionBar().setTitle(title);
     }
 
-    protected <T extends Parcelable> T getParcelableFromIntent(String key) {
-        return getIntent().getParcelableExtra(key);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action buttons
-        switch(item.getItemId()) {
-            case R.id.action_websearch:
-                // create intent to perform web search for this planet
-                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-                // catch event that there's no activity to handle intent
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /* The click listner for ListView in the navigation drawer */
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     private void selectItem(int position) {
-        // update the main content by replacing fragments
-        Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
+        String[] navigationItems = getResources().getStringArray(R.array.navigation_drawer);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        String navigationItem = navigationItems[position];
 
-        // update selected item and title, then close the drawer
+        if(navigationItem.equals(getString(R.string.home))) {
+            startActivity(HomeActivity.class);
+        } else if(navigationItem.equals(getString(R.string.schedule))) {
+            startActivity(ScheduleDashboardActivity.class);
+        } else if(navigationItem.equals(getString(R.string.speakers))) {
+            startActivity(SpeakerDashboardActivity.class);
+        } else if(navigationItem.equals(getString(R.string.twitter))) {
+
+        }
+
+
         drawerList.setItemChecked(position, true);
-        setTitle(navigationItems[position]);
         drawerLayout.closeDrawer(drawerList);
     }
-
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -201,21 +150,11 @@ public abstract class BaseActivity extends Activity {
         drawerToggle.onConfigurationChanged(newConfig);
     }
 
-    /**
-     * Fragment that appears in the "content_frame", shows a planet
-     */
-    public static class PlanetFragment extends Fragment {
-        public static final String ARG_PLANET_NUMBER = "planet_number";
+    protected <T extends Parcelable> T getParcelableFromIntent(String key) {
+        return getIntent().getParcelableExtra(key);
+    }
 
-        public PlanetFragment() {
-            // Empty constructor required for fragment subclasses
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.home, container, false);
-            return rootView;
-        }
+    private void startActivity(Class<? extends Activity> activityClass) {
+        startActivity(new Intent(this, activityClass));
     }
 }
