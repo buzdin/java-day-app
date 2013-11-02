4669409c545276fc47522294453b5be6601c38ae
Aleksey <xaotuk1@gmail.com>
2013-11-02 16:19:47 +0200

    Speaker details styling, show speaker click

--- src/main/java/lv/jug/javaday/androidapp/presentation/schedule/ScheduleDetailsFragment.java
+++ src/main/java/lv/jug/javaday/androidapp/presentation/schedule/ScheduleDetailsFragment.java
@@ -1,51 +1,110 @@
 package lv.jug.javaday.androidapp.presentation.schedule;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.InjectView;
 import lv.jug.javaday.androidapp.R;
 import lv.jug.javaday.androidapp.common.DrawableService;
 import lv.jug.javaday.androidapp.domain.Event;
+import lv.jug.javaday.androidapp.domain.Speaker;
 import lv.jug.javaday.androidapp.domain.SpeakerRepository;
 import lv.jug.javaday.androidapp.presentation.BaseFragment;
+import lv.jug.javaday.androidapp.presentation.speaker.SpeakerDetailsFragment;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class ScheduleDetailsFragment extends BaseFragment {
 
-    public static final String KEY_EVENT = "event";
+	public static final String KEY_EVENT = "event";
+	@Inject
+	DrawableService drawableService;
+	@Inject
+	SpeakerRepository speakerRepository;
+	@InjectView(R.id.speaker_photo_1)
+	ImageView speakerPhoto1;
+	@InjectView(R.id.speaker_name_1)
+	TextView speakerName1;
+	@InjectView(R.id.speaker_photo_2)
+	ImageView speakerPhoto2;
+	@InjectView(R.id.speaker_name_2)
+	TextView speakerName2;
+	@InjectView(R.id.speaker_separator)
+	ImageView speakerSeparator;
+	@InjectView(R.id.speaker_group_1)
+	LinearLayout speakerGroup1;
+	@InjectView(R.id.speaker_group_2)
+	LinearLayout speakerGroup2;
+	@InjectView(R.id.event_info)
+	TextView eventInfo;
+	@InjectView(R.id.event_title)
+	TextView eventTitle;
+	@InjectView(R.id.event_description)
+	TextView eventDescription;
 
-    @Inject
-    DrawableService drawableService;
+	@Override
+	protected int contentViewId() {
+		return R.layout.schedule_details;
+	}
 
-    @Inject
-    SpeakerRepository speakerRepository;
+	@Override
+	protected void init(Bundle bundle) {
+		Event event = getArguments().getParcelable(KEY_EVENT);
+		showEvent(event);
+	}
 
-    @InjectView(R.id.speaker_name)
-    TextView speakerName;
+	private void showEvent(Event event) {
+		eventTitle.setText(event.getTitle());
+		String eventInfoText = String.format("%s - Room %d", event.getStartingTime(), event.getRoomId());
+		eventInfo.setText(eventInfoText);
+		eventDescription.setText(event.getDescription());
 
-    @InjectView(R.id.event_title)
-    TextView eventTitle;
+		List<Speaker> speakers = speakerRepository.load(event.getSpeakerName());
 
-    @InjectView(R.id.event_description)
-    TextView eventDescription;
+		populateFirstSpeaker(speakers.get(0));
 
-    @Override
-    protected int contentViewId() {
-        return R.layout.schedule_details;
-    }
+		if (speakers.size() == 2) {
+			populateSecondSpeaker(speakers.get(1));
+		}
+	}
 
-    @Override
-    protected void init(Bundle bundle) {
-        Event event = getArguments().getParcelable(KEY_EVENT);
-        showEvent(event);
-    }
+	private void populateFirstSpeaker(final Speaker speaker) {
+		speakerName1.setText(speaker.getName());
+		speakerPhoto1.setImageDrawable(drawableService.loadDrawable(speaker.getPhoto()));
 
-    private void showEvent(Event event) {
-        String speakerId = event.getSpeakerName();
-        speakerName.setText(speakerId);
+		speakerGroup1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				showSpeaker(speaker);
+			}
+		});
+	}
 
-        eventTitle.setText(event.getTitle());
-        eventDescription.setText(event.getDescription());
-    }
+	private void populateSecondSpeaker(final Speaker speaker) {
+		speakerSeparator.setVisibility(View.VISIBLE);
+		speakerGroup2.setVisibility(View.VISIBLE);
+
+		speakerName2.setText(speaker.getName());
+		speakerPhoto2.setImageDrawable(drawableService.loadDrawable(speaker.getPhoto()));
+
+		speakerGroup2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				showSpeaker(speaker);
+			}
+		});
+	}
+
+	public void showSpeaker(Speaker speaker) {
+		Bundle data = new Bundle();
+		data.putParcelable(SpeakerDetailsFragment.KEY_SPEAKER, speaker);
+
+		SpeakerDetailsFragment fragment = new SpeakerDetailsFragment();
+		fragment.setArguments(data);
+
+		getMainActivity().changeFragment(fragment);
+	}
 }
